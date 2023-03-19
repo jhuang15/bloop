44ad9a4fdd9a506e72ef1af6c7bf89abe5e5a5ad
jhuang15 <jchuang1020@gmail.com>
2023-03-18 20:22:43 -0400

    Middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,22 @@
+import jwt from 'jwtwebtoken';
+
+export const verifyToken = async (req, res, next) => {
+  try {
+    let token = req.header("Authorization");
+
+    if (!token) {
+      return res.status(403).send("Access Denied");
+    }
+
+    if (token.startsWith("Bearer ")) {
+      token = token.slice(7, token.length).trimLeft();
+    }
+
+    const varified = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verified;
+    next();
+    
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+  }
+}
