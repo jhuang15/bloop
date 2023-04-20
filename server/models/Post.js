80528d2ede41a5db9975ba985f21cf9a5d2f5f51
jhuang15 <jchuang1020@gmail.com>
2023-04-20 18:16:42 -0400

    Complete Post Model

---
+++ server/models/Post.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const postSchema = mongogse.Schema(
+  {
+    userId: {
+      type: String,
+      required: true
+    },
+    firstName: {
+      type: String,
+      required: true
+    },
+    lastName: {
+      type: String,
+      required: true
+    },
+    location: String,
+    description: String,
+    picturePath: String,
+    userPicturePath: String,
+    // LIKES datatpye will be Map. 
+    // If a picture is liked it will be added to Map same for removing a like. 
+    // Could also use array but Map is more efficient when there are alot of likes
+    likes: {
+      type: Map, 
+      of: Boolean
+    },
+    comments: {
+      type: Array,
+      default: []
+    }
+  },
+  { timestamp: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
