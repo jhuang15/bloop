8cd9ce07c1c6151f54e984da616ed8d45643b2f3
jhuang15 <jchuang1020@gmail.com>
2023-04-20 18:50:03 -0400

    Complete Read functions in controllers

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -22,9 +22,31 @@ export const createPost = async (req, res ) => {
     //Save the post then grab the post and return
     //Hard part of frontend is consider what has to be returned from backend api and how youre sending it/formatting it, etc.
     const post = await Post.find();
-    res.status(201).json(post)
+    res.status(201).json(post) //201: successful create
      
   } catch (err) {
     res.status(409).json( { message: err.message })
+  }
+}
+
+// READ
+export const getFeedPosts = async (req, res) => {
+  try {
+    const post = await Post.find();
+    res.status(200).json(post) // 200: successful request
+
+  } catch (err) {
+    res.status(404).json( { message: err.message })
+  }
+}
+
+//This function will only send the post that match the userId
+export const getUserPosts = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const post = await Post.find( { userId });
+    res.status(200).json(post)
+  } catch {
+    res.status(404).json( { message: err.message })
   }
 }
